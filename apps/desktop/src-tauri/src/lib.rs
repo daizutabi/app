856bbcd531691a1626592ced69b538c68f71af6e
Daizu <daizutabi@gmail.com>
2024-08-13 11:21:15 +0900

    Add hello

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -1,7 +1,8 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let x = hello::add(1, 2);
+    format!("Hello, {}! You've been greeted from Rust! {}", name, x)
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
